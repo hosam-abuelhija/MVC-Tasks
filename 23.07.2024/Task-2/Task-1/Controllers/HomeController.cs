e869b3dccad6e87e0b9c6336ac7ddfacb5c3c654
hosam-abuelhija <hosam.adnan123@gmail.com>
2024-07-24 20:43:35 +0300

    Task-2

---
+++ 23.07.2024/Task-2/Task-1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SubmitContact(string name, string phone, string contactMethod, string subject, string[] services, string message)
+        {
+            ViewBag.Name = name;
+            ViewBag.Phone = phone;
+            ViewBag.ContactMethod = contactMethod;
+            ViewBag.Subject = subject;
+            ViewBag.Services = services != null ? string.Join(", ", services) : "None";
+            ViewBag.Message = message;
+
+            return View("ContactResult");
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
